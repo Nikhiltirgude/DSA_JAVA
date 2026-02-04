bc931ef22ef265dbf1b9de74a3da23fb6919432a
nikhil tirgude <tirguden@gmail.com>
2026-02-04 23:16:57 +0530

    345

---
+++ Leetcode345.java
@@ -0,0 +1,37 @@
+class Solution {
+    public String reverseVowels(String s) {
+        
+        StringBuilder sb = new StringBuilder(s);
+
+        
+        String vowels = "aeiouAEIOU";
+
+       
+        int left = 0;
+        int right = s.length() - 1;
+
+        while (left < right) {
+
+           
+            while (left < right && vowels.indexOf(sb.charAt(left)) == -1) {
+                left++;
+            }
+
+           
+            while (left < right && vowels.indexOf(sb.charAt(right)) == -1) {
+                right--;
+            }
+
+            
+            char temp = sb.charAt(left);
+            sb.setCharAt(left, sb.charAt(right));
+            sb.setCharAt(right, temp);
+
+            left++;
+            right--;
+        }
+
+       
+        return sb.toString();
+    }
+}
