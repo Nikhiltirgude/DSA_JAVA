0b35b2cae5268d576ff008664cafa301a359b4eb
nikhil tirgude <tirguden@gmail.com>
2026-03-15 22:21:13 +0530

    1512

--- Leetcode876.java
+++ Leetcode876.java
@@ -1,3 +1,29 @@
-public class Leetcode876 {
-    
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode middleNode(ListNode head) {
+        int count=0;
+        ListNode midIdx=head;
+        while(midIdx!=null){
+            count++;
+            midIdx=midIdx.next;
+        }
+        int mid=count/2;
+        ListNode temp=head;
+        while(mid>0){
+            temp=temp.next;
+            mid--;
+        }
+        
+        return temp;
+        
+    }
 }
