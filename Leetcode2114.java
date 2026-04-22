22bf96e86581382ed38f76193f62272b31895199
nikhil tirgude <tirguden@gmail.com>
2026-04-22 19:02:32 +0530

    2114

---
+++ Leetcode2114.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int mostWordsFound(String[] sentences) {
+        int max=0;
+
+        for(String s:sentences){
+            int count=1;
+            for(char ch:s.toCharArray()){
+                if(ch==' '){
+                    count++;
+                }
+            }
+            max=Math.max(count,max);
+        }
+
+        return max;
+        
+    }
+}
