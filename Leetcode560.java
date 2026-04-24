48bc57539ae54708580820bee46bffda7d9aaca1
nikhil tirgude <tirguden@gmail.com>
2026-04-24 17:54:16 +0530

    560

---
+++ Leetcode560.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int subarraySum(int[] nums, int k) {
+        Map<Integer,Integer>hm=new HashMap<>();
+        int sum=0;
+        int count=0;
+        hm.put(0,1);
+
+        for(int i=0;i<nums.length;i++){
+            sum+=nums[i];
+
+            if(hm.containsKey(sum-k)){
+                count+=hm.get(sum-k);
+            }
+
+            int oldVal=hm.getOrDefault(sum,0);
+            hm.put(sum,oldVal+1);
+        }
+        return count;
+    }
+}
