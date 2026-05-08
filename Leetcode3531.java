1e98586a8efe08058ab8c0c0291751fc5005755d
nikhil tirgude <tirguden@gmail.com>
2026-05-08 23:31:28 +0530

    3531

---
+++ Leetcode3531.java
@@ -0,0 +1,47 @@
+import java.util.*;
+
+class Solution {
+
+    public int countCoveredBuildings(int n, int[][] buildings) {
+
+        Map<Integer, Integer> rowMin = new HashMap<>();
+        Map<Integer, Integer> rowMax = new HashMap<>();
+
+        Map<Integer, Integer> colMin = new HashMap<>();
+        Map<Integer, Integer> colMax = new HashMap<>();
+
+        // build min/max maps
+        for (int[] b : buildings) {
+
+            int x = b[0];
+            int y = b[1];
+
+            rowMin.put(x, Math.min(rowMin.getOrDefault(x, Integer.MAX_VALUE), y));
+            rowMax.put(x, Math.max(rowMax.getOrDefault(x, Integer.MIN_VALUE), y));
+
+            colMin.put(y, Math.min(colMin.getOrDefault(y, Integer.MAX_VALUE), x));
+            colMax.put(y, Math.max(colMax.getOrDefault(y, Integer.MIN_VALUE), x));
+        }
+
+        int count = 0;
+
+        // check covered buildings
+        for (int[] b : buildings) {
+
+            int x = b[0];
+            int y = b[1];
+
+            boolean left  = rowMin.get(x) < y;
+            boolean right = rowMax.get(x) > y;
+
+            boolean above = colMin.get(y) < x;
+            boolean below = colMax.get(y) > x;
+
+            if (left && right && above && below) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
