62ba2f2c3f83ffbc176c6a66b64ae9b37bf5fea7
nikhil tirgude <tirguden@gmail.com>
2026-05-20 23:52:20 +0530

    3264

---
+++ Leetcode3264.java
@@ -0,0 +1,18 @@
+public class Solution {
+    public int[] getFinalState(int[] nums, int k, int multiplier) {
+        
+        
+        for(int op=0;op<k;op++){
+        int min=0;    
+        for(int i=1;i<nums.length;i++){
+            if(nums[i]<nums[min]){
+                min=i;
+            }
+        }
+        nums[min]*=multiplier;
+        }
+        return nums;
+    }
+} {
+    
+}
