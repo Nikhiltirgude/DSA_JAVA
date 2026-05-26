150748321090027a74d2164791f74193fa25c75c
nikhil tirgude <tirguden@gmail.com>
2026-05-26 22:55:27 +0530

    1475

---
+++ Leetcode1475.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] finalPrices(int[] prices) {
+        
+        int n=prices.length;
+
+        Stack<Integer>st=new Stack<>();
+
+        for(int i=0;i<n;i++){
+            while(st.size()!=0 && prices[st.peek()]>=prices[i]){
+                    
+                    prices[st.pop()] -=prices[i];
+            }
+            st.push(i);
+        }
+        return prices;
+    }
+}
