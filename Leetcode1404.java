11d27757c0d3fa5cceee45219d08667072923d0c
nikhil tirgude <tirguden@gmail.com>
2026-07-21 23:42:05 +0530

    1404

---
+++ Leetcode1404.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int numSteps(String s) {
+        int steps = 0;
+        StringBuilder sb = new StringBuilder(s);
+
+        while (sb.length() > 1) {
+            if (sb.charAt(sb.length() - 1) == '0') {
+                // even → divide by 2 (remove last bit)
+                sb.deleteCharAt(sb.length() - 1);
+            } else {
+                // odd → add 1
+                int i = sb.length() - 1;
+                while (i >= 0 && sb.charAt(i) == '1') {
+                    sb.setCharAt(i, '0');
+                    i--;
+                }
+                if (i >= 0) {
+                    sb.setCharAt(i, '1');
+                } else {
+                    sb.insert(0, '1');
+                }
+            }
+            steps++;
+        }
+        return steps;
+    }
+}
