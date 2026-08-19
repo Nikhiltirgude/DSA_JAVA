024871e920d3a408a600188e28260ffb51e16ba5
nikhil tirgude <tirguden@gmail.com>
2026-08-19 22:30:03 +0530

    1386

---
+++ Leetcode1386.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+class Solution {
+    public int maxNumberOfFamilies(int n, int[][] reservedSeats) {
+        Map<Integer, Set<Integer>> map = new HashMap<>();
+        
+        // Store reserved seats row-wise
+        for (int[] seat : reservedSeats) {
+            map.computeIfAbsent(seat[0], k -> new HashSet<>()).add(seat[1]);
+        }
+        
+        int result = 0;
+        
+        for (int row : map.keySet()) {
+            Set<Integer> reserved = map.get(row);
+            
+            boolean left = !(reserved.contains(2) || reserved.contains(3) || reserved.contains(4) || reserved.contains(5));
+            boolean middle = !(reserved.contains(4) || reserved.contains(5) || reserved.contains(6) || reserved.contains(7));
+            boolean right = !(reserved.contains(6) || reserved.contains(7) || reserved.contains(8) || reserved.contains(9));
+            
+            if (left && right) {
+                result += 2;
+            } else if (left || middle || right) {
+                result += 1;
+            }
+        }
+        
+        // Rows without reservations can fit 2 groups
+        result += (n - map.size()) * 2;
+        
+        return result;
+    }
+}
